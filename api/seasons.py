f8eb8b037442deb8820c2d960e05a277679032b4
Sailokeshg <18r01a0579@cmritonline.ac.in>
2022-11-10 16:15:27 +0530

    /season_number_episode_number endpoint implemented

--- api/seasons.py
+++ api/seasons.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from init_db import get_postgres_db
 from services.seasons import get_complete_episode_details_by_season_id
+from services.seasons import get_complete_episode_details_by_season_number_and_episode_number
 
 
 router = APIRouter(prefix='/seasons')
@@ -11,4 +12,10 @@ router = APIRouter(prefix='/seasons')
 @router.get('/{season_number}')
 async def get_episode_details(season_number: int, session: Session = Depends(get_postgres_db)):
     episode_details = await get_complete_episode_details_by_season_id(session, season_number)
+    return episode_details
+
+@router.get('/{season_number}/{episode_number}')
+async def get_episode_details(season_number: int, episode_number: int, session: Session = Depends(get_postgres_db)):
+    episode_details = await get_complete_episode_details_by_season_number_and_episode_number(session, season_number, episode_number)
+
     return episode_details
--- services/seasons.py
+++ services/seasons.py
@@ -5,3 +5,10 @@ async def get_complete_episode_details_by_season_id(session, season_number):
     if details_by_season_id == [] or (details_by_season_id is None):
         return {'error': 'No episode found for this season'}
     return details_by_season_id
+
+async def get_complete_episode_details_by_season_number_and_episode_number(session, season_number, episode_number):
+    details_by_season_id = session.query(Episode).filter_by(
+        season=season_number, episode_number_in_season=episode_number).all()
+    if details_by_season_id == [] or (details_by_season_id is None):
+        return {'error': 'No episode found for this season'}
+    return details_by_season_id
