ad90f1429794f4a32e32addbd34c0cdff5f27ec2
Sailokeshg <18r01a0579@cmritonline.ac.in>
2022-11-10 16:29:52 +0530

    /episode with minimum endpoint implemented

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
 from api.episodes import router as episodes_router
+from api.ratings import router as ratings_router
 from api.seasons import router as seasons_router
+
 app = FastAPI()
 
 app.include_router(episodes_router, tags=['Episodes'])
 app.include_router(seasons_router, tags=['Seasons'])
-
+app.include_router(ratings_router, tags=['Ratings'])
 
 @app.get('/')
 async def root():
