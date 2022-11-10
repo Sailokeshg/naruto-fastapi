6311a866464774c68420eb27f96276048ec3905f
Sailokeshg <18r01a0579@cmritonline.ac.in>
2022-11-10 15:51:11 +0530

    /episode_number endpoint implemented

--- api/episodes.py
+++ api/episodes.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from init_db import get_postgres_db
 from services.episodes import get_complete_episode_details
+from services.episodes import get_complete_episode_details_by_id
 
 
 router = APIRouter(prefix='/episodes')
@@ -11,4 +12,9 @@ router = APIRouter(prefix='/episodes')
 @router.get('/all')
 async def get_all_episode_details(session: Session = Depends(get_postgres_db)):
     episode_details = await get_complete_episode_details(session)
+    return episode_details
+
+@router.get('/{episode_number}')
+async def get_episode_details(episode_number: int, session: Session = Depends(get_postgres_db)):
+    episode_details = await get_complete_episode_details_by_id(session, episode_number)
     return episode_details
--- services/episodes.py
+++ services/episodes.py
@@ -5,3 +5,7 @@ from models.models import Episode
 async def get_complete_episode_details(session):
     details_by_id = session.execute(select(Episode)).all()
     return details_by_id
+
+async def get_complete_episode_details_by_id(session, episode_number):
+    details_by_id = session.query(Episode).get(episode_number)
+    return details_by_id
