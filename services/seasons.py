4a9d19de54c2a4f05be93fa82c5dbe12dc3eafb0
Sailokeshg <18r01a0579@cmritonline.ac.in>
2022-11-10 17:12:39 +0530

    results sorted by season and episode number

--- services/seasons.py
+++ services/seasons.py
@@ -1,7 +1,9 @@
 from models.models import Episode
 
 async def get_complete_episode_details_by_season_id(session, season_number):
-    details_by_season_id = session.query(Episode).filter_by(season=season_number).all()
+    details_by_season_id = session.query(Episode).filter_by(
+        season=season_number).order_by(
+        Episode.season, Episode.episode_number_in_season).all()
     if details_by_season_id == [] or (details_by_season_id is None):
         return {'error': 'No episode found for this season'}
     return details_by_season_id
